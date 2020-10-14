62126461fda126dbabe667b1f7aac3935e89437c
ItsmeLee <arsmagica90@gmail.com>
2020-10-15 04:01:39 +0900

    Adding a microphone license confirmation feature
    Added the function to grant the user permission to use the microphone.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-import "./styles.css";
+const recordContainer = document.getElementById('jsRecorderContainer');
+const recordingBtn = document.querySelector('.RecordingBtn');
+
+function startVoiceRecording(){
+        const stream = navigator.mediaDevices.getUserMedia({
+            audio:true
+        });
+        console.log(stream);
+}
+
+const init = () => {
+    recordingBtn.addEventListener('click',startVoiceRecording);
+}
+
+if(recordContainer){
+    init();
+}
