23acb6eb02c720e65de80ba858ec711090059db8
ItsmeLee <arsmagica90@gmail.com>
2020-10-15 22:32:30 +0900

    Add a recorded voice download function
    Added a recorded voice download function.
    
    When user click the stop recording button, the recording stop automatically and create the download link.

--- README.md
+++ README.md
@@ -5,8 +5,8 @@ Build Voice Recorder using html,css and JavaScript
 - Using the blueprint and based on what you saw on the videos, build a voice recorder.
 
 # Conditions
-- [ ] Record / Stop Recording Button. Should start/stop recording
+- [x] Record / Stop Recording Button. Should start/stop recording
 - [ ] Time indicator of the recording's progress in seconds
-- [ ] When I stop recording trigger a download the file
-- [ ] Don't use Async/Await
-- [ ] When saving the audio use {type:"audio/webm;codecs=opus"}
+- [x] When I stop recording trigger a download the file
+- [x] Don't use Async/Await
+- [x] When saving the audio use {type:"audio/webm;codecs=opus"}
--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
 
 const recordingBtn = document.querySelector('.recordingBtn');
 const constraints = {audio : true};
-const options = { mimeType : "audio/webm;codecs=opus"}
+const options = { mimeType : "audio/webm;codecs=opus"};
+let voiceRecorder;
+
+
 
 const getVoice = () => {
     const stream = navigator.mediaDevices.getUserMedia(constraints)
     .then(function(stream){
-        //console.log(stream);
-        const voiceRecorder = new MediaRecorder(stream,options);
+        voiceRecorder = new MediaRecorder(stream,options);
         voiceRecorder.addEventListener("dataavailable", handleData);
         voiceRecorder.start();
 
+
+
         recordingBtn.innerHTML = `Stop Recording`;
         recordingBtn.removeEventListener('click',getVoice);
-        recordingBtn.addEventListener('click',function(){
-            voiceRecorder.stop();
-            recordingBtn.innerHTML = `Start Recording`;
-            recordingBtn.removeEventListener('click',getVoice);
-        });
+        recordingBtn.addEventListener('click',stopRecording);
         //console.log(voiceRecorder);
     }).catch(function(error){
         console.log(error);
@@ -26,8 +26,22 @@ const getVoice = () => {
     })
 }
 
+// Stop Recording function
+const stopRecording = () => {
+    voiceRecorder.stop();
+    recordingBtn.innerHTML = `Start Recording`;
+    recordingBtn.removeEventListener('click',stopRecording);
+}
+
 const handleData = (event) => {
-    console.log(event);
+    // data 추출
+    const {data} = event;
+    // 새로운 링크 생성
+    const link = document.createElement('a');
+    link.href = URL.createObjectURL(data);
+    link.download = `recorded.webm`;
+    document.body.appendChild(link);
+    link.click();
 }
 
 
