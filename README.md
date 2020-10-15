d6276fd5cabd0ca3e4b1965db72e1eb5741f3b6b
ItsmeLee <arsmagica90@gmail.com>
2020-10-15 23:18:24 +0900

    Add recording timer
    Added recording timer function.
    
    When user click start recording button, the timer automatically show next to the button.
    And user press stop recording button, the timer disappear.

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Build Voice Recorder using html,css and JavaScript
 
 # Conditions
 - [x] Record / Stop Recording Button. Should start/stop recording
-- [ ] Time indicator of the recording's progress in seconds
+- [x] Time indicator of the recording's progress in seconds
 - [x] When I stop recording trigger a download the file
 - [x] Don't use Async/Await
 - [x] When saving the audio use {type:"audio/webm;codecs=opus"}
--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ const recordingBtn = document.querySelector('.recordingBtn');
 const constraints = {audio : true};
 const options = { mimeType : "audio/webm;codecs=opus"};
 let voiceRecorder;
-
-
+const showStatus = document.querySelector('.showStatus');
+let timer = 0;
+let showTimer;
 
 const getVoice = () => {
     const stream = navigator.mediaDevices.getUserMedia(constraints)
@@ -12,9 +13,10 @@ const getVoice = () => {
         voiceRecorder = new MediaRecorder(stream,options);
         voiceRecorder.addEventListener("dataavailable", handleData);
         voiceRecorder.start();
-
-
-
+        showTimer = setInterval(showSeconds,1000);
+        voiceRecorder.onstart = showTimer
+        
+        
         recordingBtn.innerHTML = `Stop Recording`;
         recordingBtn.removeEventListener('click',getVoice);
         recordingBtn.addEventListener('click',stopRecording);
@@ -26,15 +28,23 @@ const getVoice = () => {
     })
 }
 
+const showSeconds = () => {
+    timer++;
+    showStatus.innerHTML = `Recording for ${timer}`;
+}
+
 // Stop Recording function
 const stopRecording = () => {
     voiceRecorder.stop();
+    voiceRecorder.onstop = clearInterval(showTimer);
+    showStatus.classList.add('Q3wE');
     recordingBtn.innerHTML = `Start Recording`;
     recordingBtn.removeEventListener('click',stopRecording);
 }
 
 const handleData = (event) => {
     // data 추출
+    console.log(event);
     const {data} = event;
     // 새로운 링크 생성
     const link = document.createElement('a');
